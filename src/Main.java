6d99425f52ec6308a71015ab46c0a045c112bb13
noahchubaty-prog <noah.chubaty@stu.ocsb.ca>
2026-09-22 09:46:07 -0400

    Tuesday

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,41 @@
 import java.util.*;
+
 /*******************************
- * Name: Your name
- * Class: ICS3U
- * Date: Friday Sept. 25
- * Project Name: Restaurant
+ * Name: Your name Class: ICS3U Date: Friday Sept. 25 Project Name: Restaurant
  * 
- * You will have your first real project this week. You must meet all of the following criteria:
-Create a restaurant of your choosing
-Print menu items one at a time, including the price.
-Ask how many of each item they would like to purchase
-Must have at least 5 different menu items
-Calculate the total price
-If the total is more than $30, take off 10%.
-If the total is more than $50, take off 20%.
-Add 13% HST  to the total
-Print out the initial price, any discounts, taxes, and the grand total.
-Read in a payment amount from the user
-Calculate the change
-If the change is negative, state that they still owe you money.
-This is to be done individually.
-DO NOT use AI! 
-Feel free to use previous notes, videos, and online resources like w3schools.com
-Fork the repository, add me (MrZebarth) as a collaborator, clone the repository to your computer, program your solution, and then commit and push the results.
+ * You will have your first real project this week. You must meet all of the
+ * following criteria: Create a restaurant of your choosing Print menu items one
+ * at a time, including the price. Ask how many of each item they would like to
+ * purchase Must have at least 5 different menu items Calculate the total price
+ * If the total is more than $30, take off 10%. If the total is more than $50,
+ * take off 20%. Add 13% HST to the total Print out the initial price, any
+ * discounts, taxes, and the grand total. Read in a payment amount from the user
+ * Calculate the change If the change is negative, state that they still owe you
+ * money. This is to be done individually. DO NOT use AI! Feel free to use
+ * previous notes, videos, and online resources like w3schools.com Fork the
+ * repository, add me (MrZebarth) as a collaborator, clone the repository to
+ * your computer, program your solution, and then commit and push the results.
  ********************************/
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Welcome to the Restaurant!");
-System.out.println("The weekly specials are Fries 2$, Hot Dogs 5$, Onion Rings 3$, Water 1$, Cheeseburger 5$ ");
-System.out.println("What Will it be?");		
-String Fries = in.nextLine();
-String HotDog = in.nextLine();
+	System.out.println("Welcome to the Restaurant!");
+	System.out.println("The weekly specials are Fries 2$, Hot Dogs 5$, Onion Rings 3$, Water 1$, Cheeseburger 5$ ");
+	System.out.println("What Will it be?");
+    System.out.println("How Many Hot Dogs do you want?");
+    int HotDogs = in.nextInt();
+	System.out.println("How many Fries do you want?");
+	int fries = in.nextInt();
+	System.out.println("How many Onion Rings do you want?");
+	int OnionRings = in.nextInt();
+	System.out.println("How many Bottles of Water do you want?");
+	int Water = in.nextInt();
+	System.out.println("How many Cheeseburgers do you want?");
+	int Cheeseburgers = in.nextInt();
+	
+	
 
-		
-		
-		
-		
-		
-		
-		
 	}
 
 }
